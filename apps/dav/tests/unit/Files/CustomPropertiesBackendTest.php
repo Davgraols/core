5c925a0ccade88f1f41d2ae65f2f6291f8d3fc90
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2017-10-04 22:07:33 +0300

    Use single CustomPropertiesBackend for old and new dav endpoints

--- apps/dav/tests/unit/Files/CustomPropertiesBackendTest.php
+++ apps/dav/tests/unit/Files/CustomPropertiesBackendTest.php
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
-namespace OCA\DAV\Tests\unit\Connector\Sabre;
+namespace OCA\DAV\Tests\unit\Files;
 
 /**
  * Copyright (c) 2015 Vincent Petry <pvince81@owncloud.com>
@@ -34,7 +34,7 @@ namespace OCA\DAV\Tests\unit\Connector\Sabre;
  *
  * @group DB
  *
- * @package OCA\DAV\Tests\unit\Connector\Sabre
+ * @package OCA\DAV\Tests\unit\Files
  */
 class CustomPropertiesBackendTest extends \Test\TestCase {
 
